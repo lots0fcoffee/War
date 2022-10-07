dcc205241d8c2cb2a92e55b592e40e4b43e65c57
Mike <mikejhaze@gmail.com>
2022-10-06 21:05:07 -0400

    add logic and unit test files

---
+++ war.py
@@ -0,0 +1,189 @@
+# TODO: - Player deck not replacing from win pile
+#       - Need to clear prize lists after War is over
+
+import datetime
+import random
+import time
+import sys
+
+from numpy import integer
+
+def replace_cards(win_pile:list) -> list:
+    # I'll give 6 seconds avg for shuffling the deck
+    # time.sleep(6)
+    win_pile_copy = win_pile.copy()
+    random.shuffle(win_pile_copy)
+    return win_pile_copy
+
+def get_start_time() -> float:
+    return time.time()
+
+
+def init_deck_and_deal_cards() -> tuple:
+    deck = []
+
+    #fill the deck
+    for i in range(13):
+        for j in range(4):
+            deck.append(i)
+
+    random.shuffle(deck)
+
+    player_one = []
+    player_two = []
+
+    # give cards to players
+    counter = 0
+    for card in deck:
+        if counter%2 == 0:
+            player_one.append(card)
+        else:
+            player_two.append(card)
+        counter+=1
+
+    return player_one, player_two
+
+def play_war(player_one: list, player_two: list) -> tuple:
+    player_one_win_pile = []
+    player_two_win_pile = []
+    player_one_auto_loss = False
+    player_two_auto_loss = False
+    turns = 0
+
+    while player_one != [] and player_two != []:
+        turns += 1
+        player_one_card = player_one.pop(0)
+        player_two_card = player_two.pop(0)
+        # mimic time placing card down on table
+        #time.sleep(3)
+
+        if player_one_card > player_two_card:
+            player_one_win_pile.append(player_one_card)
+            player_one_win_pile.append(player_two_card)
+        elif player_one_card < player_two_card:
+            player_two_win_pile.append(player_one_card)
+            player_two_win_pile.append(player_two_card)
+        else:
+            # War!
+            # It probably takes about 6 seconds to play all of the cards
+            # and for the winner to collect them
+            #time.sleep(6)
+            player_one_prize = []
+            player_two_prize = []
+            player_one_play = -1
+            player_two_play = -1            
+
+            while player_one_play == player_two_play: 
+                if player_one_play != -1:
+                    player_one_prize.append(player_one_play)
+                    player_two_prize.append(player_two_play)
+
+                for i in range(2):
+                    if player_one:
+                        player_one_prize.append(player_one.pop(0))
+                    elif not player_one:
+                        # Player one doesn't have enough cards for War, auto loss
+                        if len(player_one_win_pile) <= (2-i):
+                            player_one_auto_loss = True
+                            break
+                        # Otherwise we just use our win pile
+                        player_one = replace_cards(player_one_win_pile)
+                        player_one_win_pile.clear()
+                        player_one_prize.append(player_one.pop(0))
+                if player_one:
+                    player_one_play = player_one.pop(0)
+                else:
+                    if player_one_win_pile:
+                        player_one = replace_cards(player_one_win_pile)
+                        player_one_win_pile.clear()
+                        player_one_play = player_one.pop(0)
+                    else:
+                        player_one_auto_loss = True
+                        break
+
+                for i in range(2):
+                    if player_two:
+                        player_two_prize.append(player_two.pop(0))
+                    elif not player_two:
+                        # Player two doesn't have enough cards for War, auto loss
+                        if len(player_two_win_pile) <= (2-i):
+                            player_two_auto_loss = True
+                            break
+                        # Otherwise we just use our win pile
+                        player_two = replace_cards(player_two_win_pile)
+                        player_two_win_pile.clear()
+                        player_two_prize.append(player_two.pop(0))
+                if player_two:
+                    player_two_play = player_two.pop(0)
+                else:
+                    if player_two_win_pile:
+                        player_two = replace_cards(player_two_win_pile)
+                        player_two_win_pile.clear()
+                        player_two_play = player_two.pop(0)
+                    else:
+                        player_two_auto_loss = True
+                        break
+            
+            if player_one_auto_loss or player_two_auto_loss:
+                break
+            
+            player_one_prize.append(player_one_card)
+            player_two_prize.append(player_two_card)
+            player_one_prize.append(player_one_play)
+            player_two_prize.append(player_two_play)
+            
+            if player_one_play > player_two_play:
+                player_one_win_pile.extend(player_one_prize)
+                player_one_win_pile.extend(player_two_prize)
+            else:
+                player_two_win_pile.extend(player_one_prize)
+                player_two_win_pile.extend(player_two_prize)
+            player_one_prize.clear()
+            player_two_prize.clear()            
+        
+        if player_one == []:
+            player_one = replace_cards(player_one_win_pile)
+            player_one_win_pile.clear()
+        if player_two == []:
+            player_two = replace_cards(player_two_win_pile)
+            player_two_win_pile.clear()
+
+
+    if player_one_auto_loss or player_one == []:
+        win_bit = 2
+    elif player_two_auto_loss or player_two == []:
+        win_bit = 1
+    else:
+        win_bit = -1
+    
+    return win_bit, turns
+
+def main():
+    start_time = get_start_time()
+    turn_total = 0
+    games = 100000
+    min_turns = 10000000000
+    max_turns = -1
+    for i in range(games):
+        print(i)
+        player_one, player_two = init_deck_and_deal_cards()
+        win_bit, turns = play_war(player_one, player_two)
+        turn_total += turns
+
+        if turns > max_turns:
+            max_turns = turns
+        elif turns < min_turns:
+            min_turns = turns
+    
+    turn_avg = turn_total / 100000
+
+    end_time = time.time() - start_time
+    #datetime.fromtimestamp(end_time).strftime('%Y-%m-%d')
+    if win_bit == -1:
+        sys.exit()
+
+    #print(f"Player {win_bit} wins. Game took {end_time} to complete and {turns} turns.")
+    print(f"Played {games} games, the turn avg was {turn_avg}, the max turns was {max_turns} and the min turns was {min_turns}")
+
+if __name__ == "__main__":
+    main()
---
+++ war_tests.py
@@ -0,0 +1,110 @@
+from unittest import TestCase, TestLoader, TextTestRunner, TestSuite, loader, mock
+from unittest.mock import call, MagicMock, mock_open, patch, Mock
+from war import get_start_time, replace_cards, init_deck_and_deal_cards, play_war
+import sys
+
+class test_war(TestCase):
+
+    def setUp(self) -> None:
+        global get_start_time
+        global replace_cards
+        global init_deck_and_deal_cards
+        global play_war
+
+        self.get_start_time = get_start_time
+        self.replace_cards = replace_cards
+        self.init_deck_and_deal_cards = init_deck_and_deal_cards
+        self.play_war = play_war
+
+        get_start_time = MagicMock()
+        replace_cards = MagicMock()
+        init_deck_and_deal_cards = MagicMock()
+        play_war = MagicMock()
+
+    def tearDown(self) -> None:
+        global get_start_time
+        global replace_cards
+        global init_deck_and_deal_cards
+        global play_war
+
+        get_start_time = self.get_start_time
+        replace_cards = self.replace_cards
+        init_deck_and_deal_cards = self.init_deck_and_deal_cards
+        play_war = self.play_war
+
+class test_time(test_war):
+    
+    def test_get_start_time(self):
+        time = self.get_start_time()
+        self.assertIsInstance(time, float)
+
+class test_replace_deck(test_war):
+    
+    def test_replace_cards(self):
+        l = []
+        for i in range(30):
+            l.append(i)
+        
+        cards = self.replace_cards(l)
+        self.assertTrue(len(cards) == 30)
+        self.assertNotEqual(l, cards)
+
+class test_init_hands(test_war):
+
+    def test_init_deck_and_deal_cards(self):
+        player_one_deck, player_two_deck = self.init_deck_and_deal_cards()
+
+        player_one_highest_card = -1
+        player_two_highest_card = -1
+
+        for card in player_one_deck:
+            if card > player_one_highest_card:
+                player_one_highest_card = card
+        
+        for card in player_two_deck:
+            if card > player_two_highest_card:
+                player_two_highest_card = card
+
+        self.assertEqual(len(player_one_deck), 26)
+        self.assertEqual(len(player_two_deck), 26)
+        self.assertLess(player_one_highest_card, 13)
+        self.assertLess(player_two_highest_card, 13)
+
+class test_war_run(test_war):
+
+    def test_play_war(self):
+        list_one = [2, 7, 12]
+        list_two = [5, 3, 6]
+
+        list_three = [12, 3, 4, 5, 11, 6]
+        list_four = [12, 8, 7, 9, 10, 2]
+
+        list_five = [12, 3, 4, 5, 10, 11, 7]
+        list_six = [12, 6, 7, 5, 9, 8, 5]
+
+        win_bit_one = self.play_war(list_one, list_two)
+        win_bit_two = self.play_war(list_three, list_four)
+        win_bit_three = self.play_war(list_five, list_six)
+
+        self.assertEqual(win_bit_one, 1)
+        self.assertEqual(win_bit_two, 2)
+        self.assertEqual(win_bit_three, 1)
+
+
+
+def main():
+    test_classes = [test_time, test_replace_deck, test_init_hands, test_war_run]
+
+    loader = TestLoader()
+    suites = []
+
+    for test_class in test_classes:
+        suite = loader.loadTestsFromTestCase(test_class)
+        suites.append(suite)
+        big_suite = TestSuite(suites)
+    result = TextTestRunner(verbosity=2).run(big_suite)
+    if not result.wasSuccessful():
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
